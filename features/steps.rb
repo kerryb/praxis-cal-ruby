501ee7bcf8b3e6698930b017f9533eb3a9dae4ef
Kerry Buckley <kerryjbuckley@gmail.com>
2010-01-02 21:03:09 +0000

    Properly-failing feature

--- features/steps.rb
+++ features/steps.rb
@@ -1,3 +1,5 @@
+require File.expand_path(File.dirname(__FILE__) + '/../lib/calendar.rb')
+
 When /^I run "cal ([^\"]*)"$/ do |args|
   @output = Calendar.new(args).run
 end
---
+++ lib/calendar.rb
@@ -0,0 +1,7 @@
+class Calendar
+  def initialize args
+  end
+
+  def run
+  end
+end
