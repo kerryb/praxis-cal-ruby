b017a7a66c459eed2bae6d5a5833153b9e48c195
Kerry Buckley <kerryjbuckley@gmail.com>
2010-01-02 21:47:40 +0000

    Create a separate 'cal' executable script

--- features/steps.rb
+++ features/steps.rb
@@ -1,7 +1,12 @@
 require File.expand_path(File.dirname(__FILE__) + '/../lib/calendar.rb')
 
 When /^I run "cal ([^\"]*)"$/ do |args|
-  @output = Calendar.new(args).run
+  ARGV.clear << args.split
+  orig_stdout = $stdout
+  $stdout = stdout_capture = StringIO.new
+  load File.expand_path(File.dirname(__FILE__) + '/../bin/cal')
+  $stdout = orig_stdout
+  @output = stdout_capture.string
 end
 
 Then /^I should see:$/ do |string|
