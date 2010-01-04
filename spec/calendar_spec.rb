83ccff597b6c021f1f4c3efee79c68e059710649
Kerry Buckley <kerryjbuckley@gmail.com>
2010-01-04 11:24:07 +0000

    Spec for days-of-the-week headers

--- spec/calendar_spec.rb
+++ spec/calendar_spec.rb
@@ -4,8 +4,16 @@ require "calendar"
 
 describe Calendar do
   describe "displaying a specific month and year" do
+    def get_calendar month, year
+      Calendar.new([month, year]).output
+    end
+
     it "shows the month and year in the first line" do
-      Calendar.new(%w(11 1969)).output.split("\n").first.should == "   November 1969"
+      get_calendar(11, 1969).split("\n").first.should == "   November 1969"
+    end
+
+    it "displays the days-of-the-week headers" do
+      get_calendar(11, 1969).split("\n")[1].should == "Su Mo Tu We Th Fr Sa"
     end
   end
 end
