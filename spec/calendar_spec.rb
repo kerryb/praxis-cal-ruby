2baa162313a818fd8e12add53b42b704a08b1b89
Kerry Buckley <kerryjbuckley@gmail.com>
2010-01-04 11:47:45 +0000

    Failing spec for display of days when 1st is not Sunday

--- spec/calendar_spec.rb
+++ spec/calendar_spec.rb
@@ -27,5 +27,18 @@ describe Calendar do
         ]
       end
     end
+
+    describe "when the first of the month is not a Sunday" do
+      it "displays the dates starting with 1 in the correct column" do
+        get_calendar_rows(1, 2010)[2..-1].should == [
+          "                1  2",
+          " 3  4  5  6  7  8  9",
+          "10 11 12 13 14 15 16",
+          "17 18 19 20 21 22 23",
+          "24 25 26 27 28 29 30",
+          "31"
+        ]
+      end
+    end
   end
 end
