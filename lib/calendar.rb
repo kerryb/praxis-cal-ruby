d140effc1bcbaf9f98d9053aa054e5804b7a6a81
Kerry Buckley <kerryjbuckley@gmail.com>
2010-01-10 09:25:10 +0000

    Extract month_cells method

--- lib/calendar.rb
+++ lib/calendar.rb
@@ -23,6 +23,14 @@ class Calendar
 
   def month
     output = "Su Mo Tu We Th Fr Sa\n"
+    dates = month_cells
+    until dates.empty? do
+      output << dates.slice!(0,7).map{|d| d.zero? ? "  " : "%2i" % d}.join(' ') << "\n"
+    end
+    output
+  end
+
+  def month_cells
     first_of_month = Date.new @year, @month, 1, Date::ENGLAND
     last_of_month = Date.new @year, @month, -1, Date::ENGLAND
     dates = []
@@ -30,10 +38,7 @@ class Calendar
     (first_of_month..last_of_month).each do |date|
       dates << date.day
     end
-    until dates.empty? do
-      output << dates.slice!(0,7).map{|d| d.zero? ? "  " : "%2i" % d}.join(' ') << "\n"
-    end
-    output
+    dates
   end
 
   def month_name month_number
